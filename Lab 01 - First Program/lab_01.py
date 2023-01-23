7934b67fbe7c8dd29982eaa6df60ee00fd41f2bf
David Kline <dkline2018@gmail.com>
2023-01-23 02:33:03 -0500

    Example code removed, 4 lines added

--- Lab 01 - First Program/lab_01.py
+++ Lab 01 - First Program/lab_01.py
@@ -1,2 +1,7 @@
-print("Hello World")
 print("This is my first project with python.")
+print("I'll print a \\ for an escape code.")
+print("""Here's 
+a multi-line 
+printed statement.""")
+print("Here's the last printed statement! I'll put my name in a comment below.")
+#David Kline
