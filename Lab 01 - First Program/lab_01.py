4e9f2a61107e41a9a625c8bc7f6bf7dfc7ff90e0
David Kline <dkline2018@gmail.com>
2023-01-23 02:36:29 -0500

    Re-added hello world per lab instructions

--- Lab 01 - First Program/lab_01.py
+++ Lab 01 - First Program/lab_01.py
@@ -5,3 +5,7 @@ a multi-line
 printed statement.""")
 print("Here's the last printed statement! I'll put my name in a comment below.")
 #David Kline
+
+
+print("Hello World")
+#I re-added this, as I had not committed and pushed my hello world, or the code when it had fewer lines.
