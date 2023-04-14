95684672a770b1c0873e74013c95da863fa26077
David Kline <dkline2018@gmail.com>
2023-04-13 20:54:34 -0400

    linear search "done," debugging

---
+++ Lab 10 - Spell Check/lab_10.py
@@ -0,0 +1,37 @@
+import re
+
+# This function takes in a line of text and returns
+# a list of words in the line.
+def split_line(line):
+    return re.findall('[A-Za-z]+(?:\'[A-Za-z]+)?',line)
+
+dictionary = open("dictionary.txt")
+dictionary_list = []
+for line in dictionary:
+    dictionary_line = line.strip()
+    dictionary_list.append(dictionary_line)
+dictionary.close()
+
+line_number = 0
+alice = open("AliceInWonderLand200.txt")
+for line in alice:
+    word_list = split_line(line)
+    line_number += 1
+alice.close()
+
+# read line
+# split line into list of words - use split line
+# for word in word list
+# Linear search
+# while not at end of dictionary and not found: search
+found = False
+i = 0
+for word in word_list:
+    while i < len(dictionary_list) and not found:
+        if word.upper() == dictionary_list[i]:
+            found = True
+        else: print("Line ", line_number, "Possible misspelling: ", word)
+        i += 1
+print("--- Binary Search ---")
+# Binary Search
+
