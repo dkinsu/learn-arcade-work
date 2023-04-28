069fd3576fd9020ebe714e4c3a487e232883dfd5
David Kline <dkline2018@gmail.com>
2023-04-28 02:32:07 -0400

    wizard warrior skill 2 complete

--- Lab 12 - Final Lab/final_lab_combat.py
+++ Lab 12 - Final Lab/final_lab_combat.py
@@ -1,7 +1,9 @@
 import random
 
+
 class PlayerClass:
-    def __init__(self, player_description, class_name, class_hp, class_max_hp, class_attack, mana, max_mana, p_skill1, p_skill2, p_range,
+    def __init__(self, player_description, class_name, class_hp, class_max_hp,
+                 class_attack, mana, max_mana, p_skill1, p_skill2, p_range,
                  p_skill1_desc, p_skill2_desc):
         self.player_description = player_description
         self.class_name = class_name
@@ -18,7 +20,8 @@ class PlayerClass:
 
 
 class Enemy:
-    def __init__(self, monster_description, monster_name, monster_hp, monster_attack, m_skill1, m_skill1_dmg, m_skill2, m_skill2_dmg, m_range):
+    def __init__(self, monster_description, monster_name, monster_hp, monster_attack,
+                 m_skill1, m_skill1_dmg, m_skill2, m_skill2_dmg, m_range):
         self.monster_description = monster_description
         self.monster_name = monster_name
         self.monster_hp = monster_hp
@@ -46,25 +49,27 @@ rogue = PlayerClass("A class that fights with melee and ranged weapons.\nVersati
                     'Fire a shot with a bow before performing evasive maneuvers. Creates distance. Costs 25 mana.')
 
 cube = Enemy('A BIG CUBE.', 'THE CUBE', 50, 7, 'CUBIC CONTACT', 7, 'CUBE HYPER STRIKE', 14, 1)
-
+current_enemy = None
 player_class = None
 class_select = input('What is your class of choice? ').lower()
 if 'wizard' in class_select:
-        player_class = wizard
+    player_class = wizard
 elif 'rogue' in class_select:
-        player_class = rogue
+    player_class = rogue
 elif 'warrior' in class_select:
-        player_class = warrior
+    player_class = warrior
 else:
     print('Please select a valid class.')
 damage = None
+guard = 1
 distance = 3
-while cube.monster_hp > 0:
+current_enemy = cube
+while current_enemy.monster_hp > 0:
     print('The enemy is ', distance, 'meters away.')
     command_words = input("What is your command? ").lower().split(" ")
     if command_words[0] == 'a' and player_class.p_range >= distance:
         player_class.class_attack = damage
-        cube.monster_hp -= damage
+        current_enemy.monster_hp -= damage
 
         # Mana regeneration on basic attack
         if player_class.mana < player_class.max_mana:
@@ -86,6 +91,7 @@ while cube.monster_hp > 0:
 
 # Skill 1
     elif command_words[0] == '1':
+        # Wizard
         if player_class == wizard and player_class.mana >= 40:
             player_class.class_attack *= 3
             if player_class.class_attack < 30:
@@ -97,11 +103,12 @@ while cube.monster_hp > 0:
             player_class.mana -= 40
         elif player_class == wizard and player_class.mana < 40:
             print('You don\'t have enough mana.')
+            # Warrior
         elif player_class == warrior:
             if distance <= 3 and player_class.mana >= 15:
                 print('You swing your sword destructively.')
                 damage = player_class.class_attack * 2
-                cube.monster_hp -= damage
+                current_enemy.monster_hp -= damage
                 print('You have dealt', damage, 'damage.')
                 player_class.mana -= 15
             elif distance > 3 and player_class.mana >= 15:
@@ -110,14 +117,38 @@ while cube.monster_hp > 0:
                 player_class.mana -= 15
             elif player_class.mana < 15:
                 print('You don\'t have enough mana.')
+                # Rogue
         else:
             if distance == 1 and player_class.mana >= 15:
                 print('They won\'t be prepared for this.')
-                damage = player_class.class_attack * 3
-                cube.monster_hp -= damage
+                damage = player_class.class_attack * 4
+                current_enemy.monster_hp -= damage
                 print('You have dealt', damage, 'damage.')
                 player_class.mana -= 15
             elif distance > 1 and player_class.mana >= 15:
                 print('They won\'t be prepared for this... But neither were you. You missed.')
                 print('You have dealt 0 damage.')
                 player_class.mana -= 15
+# Skill 2
+    # Wizard
+    elif command_words[0] == '2':
+        if player_class == wizard and player_class.mana >= 20:
+            if distance <= 4:
+                print('Turn them to ash.')
+                damage = player_class.class_attack * 3
+                current_enemy.monster_hp -= damage
+                print('You have dealt', damage, 'damage.')
+                player_class.mana -= 20
+            elif distance > 4:
+                print('Turn them to ash. Or try to, like you just did. You missed.')
+                player_class.mana -= 20
+            elif player_class.mana < 20:
+                print('You don\'t have enough mana.')
+    # Warrior
+    elif player_class == warrior:
+        if player_class.mana >= 10:
+            print('Your defenses are raised.')
+            guard = 3
+        elif player_class.mana < 10:
+            print('You don\'t have enough mana.')
+    # Rogue
